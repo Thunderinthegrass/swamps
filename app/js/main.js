0bfd715fa57b7228a72da24e996736018a92de47
ThunderInTheGrass <propeller8924@gmail.com>
2020-04-20 22:26:32 +0800

    доделал анимацию, прикрепил к ней ссылки

--- app/js/main.js
+++ app/js/main.js
@@ -4,16 +4,19 @@ $(function(){
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left"src="img/arrows-left.svg" alt="">',
     nextArrow: '<img class="slider-arrows slider-arrows__right"src="img/arrows-right.svg" alt="">',
-    asNavFor:'.slider-dots'
+    asNavFor:'.slider-dots',
+    // asNavFor:'.header__map'
   });
   $('.slider-dots').slick({
     asNavFor:'.header__slider',
     slidesToShow: 4,
     slidesToScroll: 1,
+    centerMode: true,
+  focusOnSelect: true
   })
-  $('.swamp-ma').slick({
-    asNavFor:'.header__slider',
-    slidesToShow: 4,
-    slidesToScroll: 1,
-  })
+  // $('.header__map').slick({
+  //   asNavFor:'.header__slider',
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  // })
 });
