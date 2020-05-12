45adadf5adafdee6667830d252dae33b0d0fdb75
ThunderInTheGrass <propeller8924@gmail.com>
2020-05-12 13:51:12 +0800

    секция swamps почти готова

--- app/js/main.js
+++ app/js/main.js
@@ -12,11 +12,20 @@ $(function(){
     slidesToShow: 4,
     slidesToScroll: 1,
     centerMode: true,
-  focusOnSelect: true
+    focusOnSelect: true
+  });
+  $('.swamps-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left"src="img/arrows-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right"src="img/arrows-right.svg" alt="">',
+    asNavFor:'.slider__map'
+  });
+  $('.slider__map').slick({
+    asNavFor:'.swamps-slider',
+    slidesToShow: 9,
+    slidesToScroll: 1,
+    centerMode: true,
+    focusOnSelect: true
   })
-  // $('.header__map').slick({
-  //   asNavFor:'.header__slider',
-  //   slidesToShow: 4,
-  //   slidesToScroll: 1,
-  // })
 });
